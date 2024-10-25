8688f631844806ac9b3948c471c7e53ed8139fa9
Giorgos Petrou <gpet88@gmail.com>
2024-10-25 15:26:25 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,10 +6,7 @@ from snowflake.snowpark.functions import col
 # Write directly to the app
 st.title(":cup_with_straw: Customize Your Smoothie :cup_with_straw:")
 st.write(
-    """Replace this example with your own code!
-    **And if you're new to Streamlit,** check
-    out our easy-to-follow guides at
-    [docs.streamlit.io](https://docs.streamlit.io).
+    """Choose the fruits you want in your current smoothie.
     """
 )
 
